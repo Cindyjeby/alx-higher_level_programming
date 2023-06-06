98cdf985654e8801551b0c8f190a834d6f27c715
cindyjeby <cindyjeby@gmail.com>
2023-06-06 14:01:15 -0700

    not q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for k in range(ord('a'), ord('z') + 1):
+for k in range(ord('a'), ord('z')+1):
     if chr(k) not in ['q', 'e']:
-        print(chr(k), end='')
+        print("{0}" .format(chr(k)), end='')
