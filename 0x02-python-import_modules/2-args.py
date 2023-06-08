eb6edc5994a1abbfc572076c3479f526ca9d4670
cindyjeby <cindyjeby@gmail.com>
2023-06-08 05:35:12 -0700

    no 2 debug

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    count = sys.argv[1:]
+    count = sys.argv
 
     if len(count) == 0:
-        print(f"0 arguments.")
-    elif len(count) == 1:
-        print(f"1arguments:")
+        print("0 arguments.")
+    elif len(count) == 2:
+        print("1 arguments:")
+        print(f"1: {count[1]}")
     else:
-        print(f"{len(count)}arguments:")
+        print(f"{len(count)-1} arguments:")
 
-        for k, arg in enumerate(count):
-            print(f"{k+1}: {arg}")
+        for k in range(1, len(count)):
+            print(f"{k}: {count[k]}")
