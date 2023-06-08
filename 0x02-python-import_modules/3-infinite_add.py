a3d9f92e4d931c0baf2e1a4c93ccb0a745f76962
cindyjeby <cindyjeby@gmail.com>
2023-06-08 05:56:51 -0700

    adds

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    total = 0
+    for k in range(1, len(sys.argv)):
+        total += int(sys.argv(k)
+     print("{:d}".format(total))
