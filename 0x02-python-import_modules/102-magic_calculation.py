5eebb27754a6642a1b976b6a51a1ef15c8ffb8fe
cindyjeby <cindyjeby@gmail.com>
2023-06-08 19:46:05 -0700

    magic

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+def magic_calculation(a, b):
+    from magic calculatio_102 import add. sub
+
+    if a < b:
+        total = add(a, b)
+        for k in range(4, 7):
+            total = add(total, k)
+        return (total)
+    else:
+        return (sub(a, b))
