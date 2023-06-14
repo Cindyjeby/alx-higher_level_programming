09dbfb332edd6aaacae842bbe1299fb85f3e2e5c
cindyjeby <cindyjeby@gmail.com>
2023-06-14 12:30:39 -0700

    average int

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not isinstance(my_list, list) of len(my_list) == 0:
+    if not my_list:
         return (0)
-    weight = 0
+    weight = []
     size = 0
-    for tup in my_list:
-        weight += (tup[0] * tup[1])
-        size += tup[1]
-        return (weight / size)
+    for total, point in my_list:
+        weight.append(total * point)
+        size += point
+        weight_average = sum(weight) / size
+        return weight_average
