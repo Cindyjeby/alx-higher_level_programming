63df431431a4bc2e7ade35059fe7b6e95700ac29
cindyjeby <cindyjeby@gmail.com>
2023-06-14 12:58:01 -0700

    debug

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None of a_dictionary == {}:
+    score = 0
+    if a_dictionary == {}:
         return None
-    score = max(a_dictionary, key=lambda g: a_dictionary[g])
-            return score
+    else:
+        for key, value in a_dictionary.items():
+            if value > score:
+                score = value
+            else:
+                continue
+            best = list({k for k in a_dictionary if a_dictionary[k] == score})
+            return best[0]
