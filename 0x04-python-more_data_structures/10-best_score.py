d138e8c38acf95c9290282c7f07a89aa071c4010
cindyjeby <cindyjeby@gmail.com>
2023-06-14 12:10:44 -0700

    debug

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    bets_int = 0
+    best_int = 0
     if a_dictionary == {}:
         return None
     else:
@@ -9,5 +9,5 @@ def best_score(a_dictionary):
                 best_int = value
             else:
                 continue
-            besy_key = list({k for k in a_dictionary if a_dictionary[k] == best_int})
+            best_key = list({k for k in a_dictionary if a_dictionary[k] == best_int})
             return best_key[0]
