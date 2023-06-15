3fad1ce017b07a0651df06707fdb3b5542d0de9c
cindyjeby <cindyjeby@gmail.com>
2023-06-15 01:15:22 -0700

    debug

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_add = set()
-    for value in my_list:
-        new_add.add(value)
-        total = sum(new_add)
-        return total
+    new_add = set(my_list)
+    total = sum(new_add)
+    return total
