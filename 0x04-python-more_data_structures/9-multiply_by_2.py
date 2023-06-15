6df686019ccc554e02f349e4a06948d8d7213c4b
cindyjeby <cindyjeby@gmail.com>
2023-06-15 03:25:57 -0700

    placement of return error

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_tionary = {}
     for key, value in a_dictionary.items():
-        mul_value = value * 2
-        new_tionary[key] = mul_value
-        return new_tionary
+        new_tionary[key] = value * 2
+    return new_tionary
