594a567500243612a8a73848e120459037ceb350
cindyjeby <cindyjeby@gmail.com>
2023-06-15 03:57:25 -0700

    delete keys debug

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for key, volume in list(a_dictionary.keys()):
-        if volume == value:
-            del a_dictionary[key]
+    deleted = []
+    for key in a_dictionary.keys():
+        if a_dictionary[key] == value:
+            deleted.append(key)
+    for key in deleted:
+        del a_dictionary[key]
     return a_dictionary
