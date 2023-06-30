fa2fb7e393bec63d56aae0e255d7f63a56d48875
cindyjeby <cindyjeby@gmail.com>
2023-06-30 06:14:10 -0700

    square debug

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,15 +2,16 @@
 
 """Define a Square"""
 
+
 class Square:
     """
     square class with a private attribute size
     and checks data type
     """
     def __int__(self, size=0):
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
 
         self.__size = size
