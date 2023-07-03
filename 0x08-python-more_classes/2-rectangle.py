bc57f2a20af1a1c90dae22b4f2969d3fd803c1cf
cindyjeby <cindyjeby@gmail.com>
2023-07-03 11:19:27 -0700

    area na dperimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""define a class"""
+
+class Rectangle:
+    """represents a class"""
+
+    def __init__(self, width=0, height=0):
+        """
+        initialises the intanace
+        width (itn): width of the rectangle
+        height (int): height of the recatngle
+        """
+        slef.width = width
+        self.height = height
+
+        @property
+        def width(self):
+            """getter for width"""
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """setter for thr widht"""
+
+            if not isinstance(value, int):
+                raise TypeError("width must be an int")
+            if vale < 0:
+            raise ValueErroe("width must be >= 0")
+        self.__width = value
+
+        @property
+        def heigth(self):
+            """setter for height"""
+            return.__height
+
+        @height.setter
+        def height(self, value):
+            """
+            setter for height
+            """
+
+            if not isinstance(value, int):
+                raise TypeError("height must be an int")
+            if value < 0:
+                raise ValueError("height must be > 0")
+            slef.__height = value
+
+            def area(self):
+                return self.width * self.height
+
+            def perimeter(self):
+                if self.width == 0 of self.height == 0:
+                    return 0
+                return (2 * self.width) + (2 * self.height)
