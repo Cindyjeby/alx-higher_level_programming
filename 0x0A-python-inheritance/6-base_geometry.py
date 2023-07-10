64d63a8569049c3776f887a20df37465c9ac6f4f
cindyjeby <cindyjeby@gmail.com>
2023-07-10 05:34:10 -0700

    exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""defines a Base_geometry class"""
+
+class BaseGeometry:
+    """reps the class BaseGeometry"""
+
+    def area(self):
+        """reps the area"""
+
+        raise Exception("area() is not implemented")
