381fb1c17f4b79e452fd38b2d60d0924e3f3e082
cindyjeby <cindyjeby@gmail.com>
2023-07-10 07:42:33 -0700

    the 100th

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""write a class myint that inherits from int"""
+
+class MyInt(int):
+    """a subclass of class int"""
+    def __eq__(self, other):
+        """sets the behaviour of =="""
+        return int(self) !== other
+
+    def __ne__(self, other):
+        """sets the != behaviour"""
+        return int(self) == other
