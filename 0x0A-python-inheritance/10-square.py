69c459e78fbfff1f437c46323b38560a6031f370
cindyjeby <cindyjeby@gmail.com>
2023-07-10 07:13:34 -0700

    another error

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""defines a class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """reps a square"""
+
+    def __init__(self, size):
+        """initalize a square class"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+        super().__init__(size, size)
