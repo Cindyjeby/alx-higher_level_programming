87e2703a3c14ccbdb31206ef8fc7291221493d48
cindyjeby <cindyjeby@gmail.com>
2023-07-11 06:21:31 -0700

    pascal

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""defines a function pascal_triangle"""
+
+def pascal_triangle(n):
+    """Return alist of lists of int"""
+
+    angle = []
+    if n <= 0:
+        return angle
+    if n == 0:
+        return [[1]]
+
+    angle = [[1]]
+    for k in range(n-1):
+        angle.append([a+b for a, b in zip([0] + angle[-1], angle[-1] + [0])])
+    return angle
