1f8d9219081aaa7bb0e49486fa1005d1316b781e
cindyjeby <cindyjeby@gmail.com>
2023-07-11 05:09:52 -0700

    returns json object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""defines a Json representation function"""
+
+import json
+
+def from_json_string(my_str):
+    """returns an object rep by a json string"""
+    return json.loads(my_str)
