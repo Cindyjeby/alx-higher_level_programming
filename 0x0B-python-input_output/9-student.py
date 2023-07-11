cb022246a7c6a3aa2f80fcc293be1e25c56be9d4
cindyjeby <cindyjeby@gmail.com>
2023-07-11 05:55:53 -0700

    student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""defines a student class"""
+
+class Student:
+    """defines Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initalize method for student"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrive a dictionary"""
+
+        return self.__dict__
