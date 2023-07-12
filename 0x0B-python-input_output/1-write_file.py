9c80bd162a6f449b85a29fdf6c8659147e70f0ad
cindyjeby <cindyjeby@gmail.com>
2023-07-12 04:33:25 -0700

    write debug

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,8 +2,9 @@
 
 """defines a function read_file"""
 
+
 def write_file(filename="", text=""):
     """writes a string to a text file"""
 
-    with open(filename, "w", encoding = "UTF -8") as myFile:
+    with open(filename, "w", encoding="UTF -8") as myFile:
         return myFile.write(text)
