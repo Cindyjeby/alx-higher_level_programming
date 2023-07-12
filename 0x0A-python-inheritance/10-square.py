a02f93d5619c8dcce57888402ca01085350c785d
cindyjeby <cindyjeby@gmail.com>
2023-07-12 05:46:14 -0700

    10th

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -3,6 +3,7 @@
 """defines a class"""
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """reps a square"""
 
@@ -12,3 +13,7 @@ class Square(Rectangle):
         self.__size = size
 
         super().__init__(size, size)
+
+    def area(self):
+        """returns area"""
+        return self.__size ** 2
