87974cedabc19069476cbf10e527506a1f747962
cindyjeby <cindyjeby@gmail.com>
2023-07-12 04:38:08 -0700

    no 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,8 +2,9 @@
 
 """defines a function that appends_file"""
 
+
 def append_write(filename="", text=""):
     """writes a string to a text file"""
 
-    with open(filename, "a", encoding = "UTF -8") as myFile:
+    with open(filename, "a", encoding ="UTF -8") as myFile:
         return myFile.write(text)
