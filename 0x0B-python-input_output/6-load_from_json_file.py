ee0a0fbaeff93065c2ca38925c07773251718df2
cindyjeby <cindyjeby@gmail.com>
2023-07-12 04:54:13 -0700

    load json

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,6 +8,6 @@ import json
 def load_from_json_file(filename):
     """create an object from a JSON file"""
 
-    with open(filename, 'r', encodeing='UTF -8') as myFile:
+    with open(filename, 'r', encoding='UTF -8') as myFile:
         json_object = json.load(myfile)
         return json_object
