efa0e41d2475e0505ec84d678cd718e40d5efd66
cindyjeby <cindyjeby@gmail.com>
2023-07-12 04:29:15 -0700

    debug

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,8 +2,9 @@
 
 """defines a function read_file"""
 
+
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout"""
 
-    with open(filename, encoding = "UTF -8") as myFile:
-        print(myFile.read(), end = "")
+    with open(filename, encoding="UTF -8") as myFile:
+        print(myFile.read(), end="")
