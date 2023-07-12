fa876cea2a1fd5471e5762210c890a1a0c3315d2
cindyjeby <cindyjeby@gmail.com>
2023-07-12 05:58:23 -0700

    space

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,6 +5,6 @@
 
 def add_attribute(obj, name, value):
     """adds a new attribute to an object"""
-    if  not hasattr(obj, '__dict__'):
+    if not hasattr(obj, '__dict__'):
         raise TypeError("can't add new attribute")
     setattr(obj, name, value)
