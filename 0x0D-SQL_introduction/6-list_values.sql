f4c90ece306cf7ab117ed736abbf5424a6365b7c
cindyjeby <cindyjeby@gnail.com>
2023-08-15 04:22:39 -0700

    listValues

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- Script that lists all rows of the table first_table from the database hbtn_0c_0 in the MySQL server
+-- All field should be printed
+-- Database name will be passed as an argument
+SELECT * FROM first_table;
