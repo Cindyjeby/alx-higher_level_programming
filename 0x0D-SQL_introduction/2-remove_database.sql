08e569f4a8ea1656095e47ec2b72ff693c308af7
cindyjeby <cindyjeby@gnail.com>
2023-08-15 04:03:55 -0700

    typo

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
---script that deletes the data base hbtn_0c_0 in the MySQL server
-DROP DATABASE IF EXIST hbtn_0c_0
+-- Script that deletes the data base hbtn_0c_0 in the MySQL server
+DROP DATABASE IF EXISTS hbtn_0c_0
