0bc4afc60593ae137bf1f515873587ab39cc67e0
cindyjeby <cindyjeby@gnail.com>
2023-08-15 04:48:29 -0700

    oder

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Script that lists all records of a table second_table of the database hbtn_0c_0 in the MySQL server
+-- Should display both the score and the name 
+-- Records should ordered by score(top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
