70cb2e873dd027ebe5c130c927ad3941599a3692
cindyjeby <cindyjeby@gnail.com>
2023-08-16 07:14:13 -0700

    hbtn

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,6 +6,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	state_id INT,
-	FOREIGN KEY (state_id) REFERENCES hbnt_0d_usa.states(id),
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
 	name VARCHAR(256) NOT NULL
 	);
