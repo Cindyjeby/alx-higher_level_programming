33866f3627664014b3884b89f332db71f60abcdc
cindyjeby <cindyjeby@gnail.com>
2023-08-16 06:15:22 -0700

    creates user pswd

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+--Script that creates the MySQL server user User_0d_1
+--the user password should be set to user_0d_1_pwd
+--if the user already exists the script shouldnot fail
+CREATE USER
+IF NOT EXISTS 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_od_1_pwd';
+	GRANT ALL PRIVILEGES
+	ON *.*
+	TO 'user_0d_1'@'localhost';
