83ceacfaee27b13c64084f430e7c6ac1825ec46e
cindyjeby <cindyjeby@gnail.com>
2023-08-16 07:35:20 -0700

    shoes

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,7 +3,7 @@
 -- Results must be dorted inascending order by tv_shows.title and tv_show_genre.genre_id
 -- Only one SELECT statement should be used
 
-SELECT tv_show.title, tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
-INNER JOIN tv_shows ON tv_show.id = tv_show_genres.show_id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show-genres.genre_id ASC;
