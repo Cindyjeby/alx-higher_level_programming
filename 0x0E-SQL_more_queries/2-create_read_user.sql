2af91c223ee1148c862d47291c06dc450ea8c6ef
cindyjeby <cindyjeby@gnail.com>
2023-08-16 06:41:34 -0700

    userread

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,11 +3,6 @@
 --user passward should be set to user_0d_2_pwd
 --if database already exists the script should not fail
 --if the user already exists the script should not fail
-CREATE DATABASE
-IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER
-IF NOT EXISTS 'user_0d_2'@'localhost'
-	IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-ON `hbnt_0d_2`.*
-TO 'user_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbnt_0d_2.* TO 'user_0d_2'@'localhost';
