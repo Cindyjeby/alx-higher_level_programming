ff9d0f21364f73388356b77c446c6f5fca85ac2b
cindyjeby <cindyjeby@gnail.com>
2023-09-12 06:40:14 -0700

    reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-export.esrever = function (list) {
-	let myVar = '';
+exports.esrever = function (list) {
+  let myVar = '';
 
-	for (let k = 0; k < list.length / 2; k++) {
-		myVar = list[k];
-		list[k] = list[list.length - i - 1];
-		list[list.length - i - 1] = myVar;
-	}
-	return list;
+  for (let k = 0; k < list.length / 2; k++) {
+    myVar = list[k];
+    list[k] = list[list.length - k - 1];
+    list[list.length - k - 1] = myVar;
+  }
+  return list;
 };
