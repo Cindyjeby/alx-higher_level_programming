44adc6715a5553a68f2046e8e09bb2df5e17bca9
cindyjeby <cindyjeby@gnail.com>
2023-09-17 23:37:52 -0700

    nullable

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""file that contains the class definition of a state and an instance"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """class def of a state"""
+
+    __tablename__ = "states"
+    id = Column(Integer, primary_key= True, autoincrement=True,
+            nullable=False)
+    name = Column(String(128), nullable=False)
