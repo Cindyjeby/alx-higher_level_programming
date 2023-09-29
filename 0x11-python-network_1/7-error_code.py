59dcaaebd4830e4c4610c4535838ebb9449fcc51
cindyjeby <cindyjeby@gnail.com>
2023-09-29 15:28:33 -0700

    debug

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     url = sys.argv[1]
     response = requests.get(url)
     if (response.status_code >= 400):
-        print(f"Errorcode: {response.status_code}")
+        print(f"Error code: {response.status_code}")
     else:
         print(response.text)
