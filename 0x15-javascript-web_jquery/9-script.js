4b7d33b3263a7cca1049c040d703cab9aa5773a9
cindyjeby <cindyjeby@gmail.com>
2023-11-01 12:11:36 -0700

    say hello

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,9 @@
+i$.ajax({
+	url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+	method: 'GET',
+	dataType: 'json',
+	success: function (data) {
+		const translation = data.hello;
+		$('DIV#hello').text(translation);
+	}
+});
